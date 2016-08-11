04e9734e78b37b4cdcb4cbee34e8925df2739698
jayaram87 <jayaramraja1987@gmail.com>
2016-08-11 03:22:39 +0000

    completed

--- app/controllers/collaborations_controller.rb
+++ app/controllers/collaborations_controller.rb
@@ -1,16 +1,18 @@
 class CollaborationsController < ApplicationController
+    
     def create
-       @collaboration = Collaboration.new
-       @collaboration.user = params[:collaboration][:user_id]
-       @collaboration.wiki = params[:collaboration][:wiki_id]
-       @collaboration.update(user_id: @collaboration.user, wiki_id: @collaboration.wiki)
-       redirect_to root_path
+        user = User.find(params[:user_id])
+        wiki = Wiki.find(params[:wiki_id])
+        wiki.collaborators << user
+        redirect_to wiki
     end
     
     def destroy
-        @collaboration = Collaboration.find(params[:id])
+        wiki = Wiki.find(params[:wiki_id])
+        user = User.find(params[:user_id])
+        @collaboration = Collaboration.find_by(user_id: user.id, wiki_id: wiki.id)
         @collaboration.destroy
-        redirect_to root_path
+        redirect_to wiki
     end
     
 end
